952fe8f3d07786c3e027768cd99d63dae7976067
POLICE DILIP <policedilip420@gmail.com>
2022-11-24 12:41:40 +0530

    Add project files.

---
+++ SNP-Assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SNP_Assignment;
+
+public class Program
+{
+
+    static void Main(string[] args)
+    {
+        IServiceCollection services = new ServiceCollection();
+
+        IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetParent(AppContext.BaseDirectory).FullName)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        services.AddSingleton<IConfiguration>(configuration);
+        services.AddSingleton<UploadAndDownloadFiles>();
+
+        var serviceProvider = services.BuildServiceProvider();
+        var uploadAndDownloadFiles = serviceProvider.GetService<UploadAndDownloadFiles>();
+
+        uploadAndDownloadFiles.UploadFiles();
+
+    }
+
+}
---
+++ SNP-Assignment/UploadAndDownloadFiles.cs
@@ -0,0 +1,55 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNP_Assignment
+{
+    public class UploadAndDownloadFiles
+    {
+        private readonly IConfiguration _configuration;
+
+        public UploadAndDownloadFiles(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+
+        public void UploadFiles()
+        {
+            string BlobConnectionString = _configuration.GetSection("BlobConnectionString").Value;
+            string BlobContainerName = _configuration.GetSection("BlobContainerName").Value;
+            string FilesDownloadToPath = _configuration.GetSection("FilesDownloadToPath").Value;
+            string FilesUploadFromPath = _configuration.GetSection("FilesUploadFromPath").Value;
+
+            BlobServiceClient blobServiceClient = new BlobServiceClient(BlobConnectionString);
+            BlobContainerClient blobContainerClient = blobServiceClient.CreateBlobContainer(BlobContainerName);
+
+            var files = Directory.GetFiles(FilesUploadFromPath);
+            foreach (var file in files)
+            {
+                using (MemoryStream ms = new MemoryStream(File.ReadAllBytes(file)))
+                {
+                    blobContainerClient.UploadBlob(Path.GetFileName(file), ms);
+                }
+                Console.WriteLine(file + "Uploaded!");
+            }
+
+
+            var containerClient = blobServiceClient.GetBlobContainerClient(BlobContainerName);
+            var blobs = containerClient.GetBlobs();
+            foreach (var blob in blobs)
+            {
+                Console.WriteLine(blob.Name);
+                BlobClient blobClient = containerClient.GetBlobClient(blob.Name);
+                blobClient.DownloadTo(FilesDownloadToPath + blob.Name);
+            }
+
+
+
+        }
+    }
+}
